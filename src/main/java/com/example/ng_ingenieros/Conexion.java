ad169ec43740485dd570e665953f8570f046c1cf
DANIEL SALGUERO <20220270@ricaldone.edu.sv>
2023-11-19 19:46:33 -0600

    Boton de inicio ya muestra paneles de proyectos

--- src/main/java/com/example/ng_ingenieros/Conexion.java
+++ src/main/java/com/example/ng_ingenieros/Conexion.java
@@ -10,9 +10,8 @@ public class Conexion {
     }
 // cambiar el server name por el que les aparezca en sqlserver.
 
-    //private static final String URL = "jdbc:sqlserver://localhost\\DESKTOP-LSV5E7C:53617;databaseName=NGingenierosBD;encrypt=false;trustServerCertificate=true;";
-    //private static final String URL = "jdbc:sqlserver://localhost\\LAPTOP-J5ID0GJ2:51100;databaseName=NGingenierosBD;encrypt=false;trustServerCertificate=true;";
-    private static final String URL = "jdbc:sqlserver://localhost\\LAPTOP-FCPSFI90:1433;databaseName=NGingenierosBD;encrypt=false;trustServerCertificate=true;";
+    //private static final String URL = "jdbc:sqlserver://localhost\\DESKTOP-KD9DVMM:63886;databaseName=NGingenierosBD;encrypt=false;trustServerCertificate=true;";
+    private static final String URL = "jdbc:sqlserver://localhost\\LAPTOP-J5ID0GJ2:51100;databaseName=NGingenierosBD;encrypt=false;trustServerCertificate=true;";
     private static final String USUARIO = "sa";
     private static final String CONTRASENA = "itr23";  //cambiar la contraseña por la de sus usuarios de sql
 
--- src/main/java/com/example/ng_ingenieros/HelloApplication.java
+++ src/main/java/com/example/ng_ingenieros/HelloApplication.java
@@ -5,16 +5,40 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.StageStyle;
+
+import java.sql.*;
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
 
     @Override
     public void start(Stage stage) throws IOException {
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400); //width, heigth
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuPrincipal.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600); //width, heigth
         stage.setTitle("NG Ingenieros");
         stage.setScene(scene);
         stage.show();
@@ -28,3 +52,4 @@ public class HelloApplication extends Application {
     }
 
 }
+
