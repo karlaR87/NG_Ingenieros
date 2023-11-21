69abf752b11f78b35a3f3b276585e7fd8b01193e
DANIEL SALGUERO <20220270@ricaldone.edu.sv>
2023-11-20 22:24:19 -0600

    Mejora en la vista de los paneles

--- src/main/java/com/example/ng_ingenieros/HelloApplication.java
+++ src/main/java/com/example/ng_ingenieros/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuPrincipal.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 500); //width, heigth
+        Scene scene = new Scene(fxmlLoader.load(), 700, 450); //width, heigth
         stage.setTitle("NG Ingenieros");
         stage.setScene(scene);
         stage.show();
