1eff28e54942b7740b6c0ee2e9b2da6ecb92473b
ElmerRales <20220240@ricaldone.edu.sv>
2023-12-14 19:39:45 -0600

    borrador de cargos y bancos menu

--- src/main/java/com/example/ng_ingenieros/Controlador/AdministrarControlador.java
+++ src/main/java/com/example/ng_ingenieros/Controlador/AdministrarControlador.java
@@ -11,13 +11,30 @@ import javafx.stage.Stage;
 public class AdministrarControlador {
     @FXML
     private Button btnBancos;
+    @FXML
+    private Button btncargos;
     public void initialize() {
         // Configura el evento de clic para el botón
         btnBancos.setOnAction(this::btnBancosOnAction);
+        btncargos.setOnAction(this::btncargosOnAction);
     }
     private void btnBancosOnAction(ActionEvent event){
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ng_ingenieros/CrudBancos.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Nueva");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void btncargosOnAction(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ng_ingenieros/CrudCargos.fxml"));
             Parent root = loader.load();
 
             Stage stage = new Stage();
